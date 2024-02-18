6010351fa2c2764e931b041e3a2a02693a129c72
Kai Schnittcher <kai@deinsmarthome.info>
2024-02-18 19:51:39 +0100

    Fix, wenn Tesla im Standby ist.

--- TeslaVehicleGUISettings/module.php
+++ TeslaVehicleGUISettings/module.php
@@ -9,14 +9,14 @@ class TeslaVehicleGUISettings extends IPSModuleStrict
     use TeslaHelper;
     use VariableProfileHelper;
 
-    public function Create() : void
+    public function Create(): void
     {
         //Never delete this line!
         parent::Create();
 
         $this->ConnectParent('{D5994951-CD92-78B7-A059-3D423FCB599A}');
 
-        $this->RegisterPropertyString('VIN','');
+        $this->RegisterPropertyString('VIN', '');
         $this->RegisterPropertyInteger('Interval', 60);
 
         $this->RegisterVariableBoolean('gui_24_hour_time', $this->Translate('GUI 24 Hour Time'));
@@ -29,31 +29,33 @@ class TeslaVehicleGUISettings extends IPSModuleStrict
         $this->RegisterTimer('Tesla_UpdateGUISettings', 0, 'Tesla_FetchData($_IPS[\'TARGET\']);');
     }
 
-    public function Destroy() : void
+    public function Destroy(): void
     {
         $this->UnregisterTimer('Tesla_UpdateGUISettings');
     }
 
-    public function ApplyChanges() : void
+    public function ApplyChanges(): void
     {
 
         //Never delete this line!
         parent::ApplyChanges();
     }
 
-    public function FetchData() :void
+    public function FetchData(): void
     {
         $response = json_decode($this->SendDataToParent(json_encode([
             'DataID'   => '{FB4ED52F-A162-6F23-E7EA-2CBAAF48E662}',
             'Endpoint' => '/api/1/vehicles/' . $this->ReadPropertyString('VIN') . '/vehicle_data',
             'Payload'  => ''
-        ])))->response->gui_settings;
+        ])));
 
-        foreach ($response as $key => $Value) {
-            if (@$this->GetIDForIdent($key) != false) {
-                $this->SetValue($key, $Value);
-            } else {
-                $this->SendDebug('Variable not exist', 'Key: ' . $key . ' - Value: ' . $Value, 0);
+        if ($respone->response != null) {
+            foreach ($response->response->gui_settings as $key => $Value) {
+                if (@$this->GetIDForIdent($key) != false) {
+                    $this->SetValue($key, $Value);
+                } else {
+                    $this->SendDebug('Variable not exist', 'Key: ' . $key . ' - Value: ' . $Value, 0);
+                }
             }
         }
     }
