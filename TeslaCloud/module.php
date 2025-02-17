1d54d15b29223b4c667fc60c335593d6509c008f
Michael Maroszek <maroszek@gmx.net>
2025-02-17 23:17:06 +0100

    rework module to use own ClientID/ClientSecret against new Telsa API requirements

--- TeslaCloud/module.php
+++ TeslaCloud/module.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 class TeslaCloud extends IPSModuleStrict
 {
-    private $oauthIdentifer = 'tesla';
+    private $oauthIdentifier = 'tesla';
     private $oauthServer = 'oauth.ipmagic.de';
 
     public function Create(): void
@@ -12,9 +12,13 @@ class TeslaCloud extends IPSModuleStrict
         //Never delete this line!
         parent::Create();
 
-        $this->RegisterAttributeString('Token', '');
+        $this->RegisterPropertyString('ClientID', '');
+        $this->RegisterPropertyString('ClientSecret', '');
 
-        $this->RegisterOAuth($this->oauthIdentifer);
+        $this->RegisterAttributeString('Token', '');
+        $this->RegisterAttributeString('PrivateKey', '');
+
+        $this->RegisterOAuth($this->oauthIdentifier);
     }
 
     public function ApplyChanges(): void
@@ -44,7 +48,7 @@ class TeslaCloud extends IPSModuleStrict
     public function GetConfigurationForm(): string
     {
         $data = json_decode(file_get_contents(__DIR__ . '/form.json'));
-        $data->elements[1]->caption = $this->ReadAttributeString('Token') ? 'Token: ' . substr($this->ReadAttributeString('Token'), 0, 16) . '...' : $this->Translate('Token: Not registered yet');
+        $data->actions[1]->caption = $this->ReadAttributeString('Token') ? 'Token: ' . substr($this->ReadAttributeString('Token'), 0, 16) . '...' : $this->Translate('Token: Not registered yet');
         return json_encode($data);
     }
 
@@ -53,9 +57,120 @@ class TeslaCloud extends IPSModuleStrict
      */
     public function Register(): string
     {
+        if (!$this->ReadPropertyString("ClientID") || !$this->ReadPropertyString("ClientSecret")) {
+            echo $this->Translate('Please register a new app with Tesla to get a ClientID/ClientSecret!');
+            return "";
+        }
 
         //Return everything which will open the browser
-        return 'https://' . $this->oauthServer . '/authorize/' . $this->oauthIdentifer . '?username=' . urlencode(IPS_GetLicensee());
+        $query = http_build_query([
+            'username' => IPS_GetLicensee(),
+            'scope' => 'openid offline_access vehicle_device_data vehicle_cmds vehicle_charging_cmds vehicle_location',
+            'clientid' => $this->ReadPropertyString("ClientID"),
+            'clientsecret' => $this->ReadPropertyString("ClientSecret"),
+        ]);
+        return 'https://' . $this->oauthServer . '/authorize/' . $this->oauthIdentifier . '?' . $query;
+    }
+
+    public function RegisterDomain(): void
+    {
+        if (!$this->ReadPropertyString("ClientID") || !$this->ReadPropertyString("ClientSecret")) {
+            echo $this->Translate('Please register a new app with Tesla to get a ClientID/ClientSecret!');
+            return "";
+        }
+
+        $specialFolder = IPS_GetKernelDir() . '.well-known/appspecific';
+        $specialFile = "com.tesla.3p.public-key.pem";
+
+        //Create special folder, that Telsa is requiring
+        if (!is_dir($specialFolder)) {
+            mkdir($specialFolder, 0777, true);
+        }
+
+        //Create certificate and put into this folder
+        //https://developer.tesla.com/docs/fleet-api/getting-started/what-is-fleet-api#step-3-generate-a-public-private-key-pair
+        if (!file_exists($specialFolder . "/" . $specialFile)) {
+                $this->UpdateFormField("KeyGenerationProgress", "visible", true);
+
+                // Generate a new EC key pair using secp256r1
+                $config = array(
+                    "private_key_bits" => 512,
+                    "private_key_type" => OPENSSL_KEYTYPE_EC,
+                    "curve_name" => "prime256v1",
+                    'config' => __DIR__ . '/openssl.cnf'
+                );
+
+                $privateKey = openssl_pkey_new($config);
+
+                if (!$privateKey) {
+                    die("Error generating key pair: " . openssl_error_string());
+                }
+
+                // Export to PEM
+                openssl_pkey_export($privateKey, $privateKeyPEM);
+
+                // Put the PEM-encoded private key into our attribute
+                $this->WriteAttributeString('PrivateKey', $privateKeyPEM);
+
+                // Extract the public key
+                $keyDetails = openssl_pkey_get_details($privateKey);
+                $publicKeyPEM = $keyDetails['key'];
+
+                // Put PEM-encoded public key into the special file
+                file_put_contents($specialFolder . "/" . $specialFile, $publicKeyPEM);
+
+                // Free the key resource
+                openssl_free_key($privateKey);
+
+                $this->UpdateFormField("KeyGenerationProgress", "visible", false);
+        }
+
+        //Exchange our Authentication Code for a permanent Refresh Token and a temporary Access Token
+        $options = [
+            'http' => [
+                'header'        => "Content-Type: application/x-www-form-urlencoded\r\n",
+                'method'        => 'POST',
+                'content'       => http_build_query([
+                    'grant_type'    => 'client_credentials',
+                    'client_id'     => $this->ReadPropertyString("ClientID"),
+                    'client_secret' => $this->ReadPropertyString("ClientSecret"),
+                    'scope'         => 'openid vehicle_device_data vehicle_cmds vehicle_charging_cmds',
+                    'audience'      => 'https://fleet-api.prd.na.vn.cloud.tesla.com',
+                ]),
+                'ignore_errors' => true
+            ]
+        ];
+        $context = stream_context_create($options);
+        $result = file_get_contents('https://fleet-auth.prd.vn.cloud.tesla.com/oauth2/v3/token', false, $context);
+
+        $data = json_decode($result);
+
+        if (!isset($data->token_type) || strtolower($data->token_type) != 'bearer') {
+            die('Bearer Token expected. Got: ' . $result);
+        }
+
+        $content = json_encode([
+            "domain" => str_replace("https://", "", CC_GetUrl(IPS_GetInstanceListByModuleID('{9486D575-BE8C-4ED8-B5B5-20930E26DE6F}')[0])),
+        ]);
+
+        $opts = [
+            'http' => [
+                'method'        => 'POST',
+                'header'        => 'Authorization: Bearer ' . $data->access_token . "\r\n" . 'Content-Type: application/json' . "\r\n" . 'Content-Length: ' . strlen($content) . "\r\n",
+                'content'       => $content,
+                'ignore_errors' => true,
+            ],
+        ];
+        $context = stream_context_create($opts);
+
+        $result = file_get_contents('https://fleet-api.prd.eu.vn.cloud.tesla.com/api/1/partner_accounts', false, $context);
+
+        if ((strpos($http_response_header[0], '200') === false)) {
+            echo $result;
+            return;
+        }
+
+        echo "OK";
     }
 
     /**
@@ -93,6 +208,11 @@ class TeslaCloud extends IPSModuleStrict
     {
         $this->SendDebug('FetchRefreshToken', 'Use Authentication Code to get our precious Refresh Token!', 0);
 
+        $query = http_build_query([
+            'clientid' => $this->ReadPropertyString("ClientID"),
+            'clientsecret' => $this->ReadPropertyString("ClientSecret"),
+        ]);
+
         //Exchange our Authentication Code for a permanent Refresh Token and a temporary Access Token
         $options = [
             'http' => [
@@ -103,7 +223,7 @@ class TeslaCloud extends IPSModuleStrict
             ]
         ];
         $context = stream_context_create($options);
-        $result = file_get_contents('https://' . $this->oauthServer . '/access_token/' . $this->oauthIdentifer, false, $context);
+        $result = file_get_contents('https://' . $this->oauthServer . '/access_token/' . $this->oauthIdentifier . "?" . $query, false, $context);
 
         $data = json_decode($result);
 
@@ -136,6 +256,11 @@ class TeslaCloud extends IPSModuleStrict
 
             $this->SendDebug('FetchAccessToken', 'Use Refresh Token to get new Access Token!', 0);
 
+            $query = http_build_query([
+                'clientid' => $this->ReadPropertyString("ClientID"),
+                'clientsecret' => $this->ReadPropertyString("ClientSecret"),
+            ]);
+
             //If we slipped here we need to fetch the access token
             $options = [
                 'http' => [
@@ -146,7 +271,7 @@ class TeslaCloud extends IPSModuleStrict
                 ]
             ];
             $context = stream_context_create($options);
-            $result = file_get_contents('https://' . $this->oauthServer . '/access_token/' . $this->oauthIdentifer, false, $context);
+            $result = file_get_contents('https://' . $this->oauthServer . '/access_token/' . $this->oauthIdentifier . "?" . $query, false, $context);
 
             $data = json_decode($result);
 
